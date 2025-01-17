b1753580aea2f98de62a2bc57753204cd9ca8af5
Sachin Sharma <ss0382245@gmail.com>
2025-01-17 15:50:33 +0530

    adding the currancy formatter and creating the custom button component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import "../../assets/CSS/Header.css"
 import logo from "../../assets/logo.jpg"
+import Button from "../button/Button"
 
 export default function Header() {
   return (
@@ -9,7 +10,7 @@ export default function Header() {
             <h1 >Order Food</h1>
         </div>
         <div>
-            <button>Cart (0)</button>
+            <Button textOnly> Cart(0)</Button>
         </div>
     </header>
   )
--- src/components/meals/MealItems.jsx
+++ src/components/meals/MealItems.jsx
@@ -1,5 +1,6 @@
 import "../../assets/CSS/MealsItem.css"
-
+import {currencyFormatter} from "../../utils/Formatting"
+import Button from "../button/Button"
 export default function MealItems({ meal }) {
     return (
         <li className="meal-item">
@@ -7,11 +8,11 @@ export default function MealItems({ meal }) {
                 <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
                 <div>
                     <h3>{meal.name}</h3>
-                    <p className="meal-item-price">{meal.price}</p>
+                    <p className="meal-item-price">{ currencyFormatter.format(meal.price) }</p>
                     <p className="meal-item-description">{meal.description}</p>
                 </div>
                 <div className="meal-item-actions">
-                    <button>Add to Cart</button>
+                    <Button>Add to Cart</Button>
                 </div>
             </article>
         </li>
